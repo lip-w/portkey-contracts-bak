e57097d24618b9edaeb7c1b301a81c9f74d193f2
kevin <kevin@portkey.finance>
2023-03-08 13:44:03 +0800

    Remove distinct in ValidateLoginGuardian

--- contract/Portkey.Contracts.CA/CAContract_SyncCAHolder.cs
+++ contract/Portkey.Contracts.CA/CAContract_SyncCAHolder.cs
@@ -35,7 +35,7 @@ public partial class CAContract
         loginGuardians.AddRange(holderInfo.GuardianList.Guardians.Where(g => g.IsLoginGuardian)
             .Select(g => g.IdentifierHash));
 
-        var loginGuardianIdentifierHashList = loginGuardianInput.Distinct().ToList();
+        var loginGuardianIdentifierHashList = loginGuardianInput.ToList();
 
         Assert(loginGuardians.Count == loginGuardianIdentifierHashList.Count,
             "The amount of LoginGuardianInput not equals to HolderInfo's LoginGuardians");
