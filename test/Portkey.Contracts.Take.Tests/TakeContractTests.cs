7c3829710bf5673bd9857cf725a94d5a05187faf
felix-zhaolei <felix@portkey.finance>
2023-04-04 20:15:40 +0800

    feat: take ut.

--- test/Portkey.Contracts.Take.Tests/TakeContractTests.cs
+++ test/Portkey.Contracts.Take.Tests/TakeContractTests.cs
@@ -2,6 +2,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using AElf.Contracts.MultiToken;
 using AElf.ContractTestBase.ContractTestKit;
+using AElf.Cryptography.ECDSA;
+using AElf.Types;
 using Google.Protobuf.WellKnownTypes;
 using Portkey.Contracts.Take.Tests;
 using Shouldly;
@@ -11,42 +13,123 @@ namespace Portkey.Contracts.Take.Tests;
 
 public class TakeContractTests : TakeContractTestBase
 {
-    [Fact]
-    public async Task PipelineTest()
+    private TakeContractContainer.TakeContractStub AdminStub { get; set; }
+    private TakeContractContainer.TakeContractStub UserStub { get; set; }
+    private TokenContractContainer.TokenContractStub AdminTokenStub { get; set; }
+    private TokenContractContainer.TokenContractStub UserTokenStub { get; set; }
+
+    public TakeContractTests()
     {
-        // Get a stub for testing.
         var keyPair = SampleAccount.Accounts.First().KeyPair;
-        var adminStub = GetTakeContractStub(keyPair);
-        var adminTokenStub = GetTokenContractStub(keyPair);
-        var userStub = GetTakeContractStub(SampleAccount.Accounts.Skip(1).First().KeyPair);
-        var userTokenStub = GetTokenContractStub(SampleAccount.Accounts.Skip(1).First().KeyPair);
+        var keyPairNext = SampleAccount.Accounts.Skip(1).First().KeyPair;
+        AdminStub = GetTakeContractStub(keyPair);
+        AdminTokenStub = GetTokenContractStub(keyPair);
+        UserStub = GetTakeContractStub(keyPairNext);
+        UserTokenStub = GetTokenContractStub(keyPairNext);
+    }
 
-        await adminStub.Initialize.SendAsync(new InitializeInput());
+    [Fact]
+    public async Task Initialize_Test()
+    {
+        await AdminStub.Initialize.SendAsync(new InitializeInput());
 
-        // User failed to take.
         {
-            var executionResult = await userStub.Take.SendWithExceptionAsync(new TakeInput
+            var executionResult = await UserStub.Take.SendWithExceptionAsync(new TakeInput
             {
                 Symbol = "ETH",
                 Amount = 100_00000000
             });
             executionResult.TransactionResult.Error.ShouldContain("Invalid symbol.");
         }
+    }
 
-        // User takes.
-        // {
-        //     await userStub.Take.SendAsync(new TakeInput
-        //     {
-        //         Symbol = "ELF",
-        //         Amount = 100_00000000
-        //     });
-        //     // Check user balance.
-        //     var balance = (await adminTokenStub.GetBalance.CallAsync(new GetBalanceInput
-        //     {
-        //         Owner = SampleAccount.Accounts.Skip(1).First().Address,
-        //         Symbol = "ELF"
-        //     })).Balance;
-        //     balance.ShouldBe(100_00000000);
-        // }
+    [Fact]
+    public async Task Take_Test()
+    {
+        await AdminStub.Initialize.SendAsync(new InitializeInput());
+
+        await AdminTokenStub.Transfer.SendAsync(new TransferInput
+        {
+            Amount = 200_00000000,
+            Memo = "hi",
+            Symbol = "ELF",
+            To = Address.FromBase58("2LUmicHyH4RXrMjG4beDwuDsiWJESyLkgkwPdGTR8kahRzq5XS")
+        });
+
+        await UserStub.Take.SendAsync(new TakeInput
+        {
+            Symbol = "ELF",
+            Amount = 100_00000000
+        });
+        // Check user balance.
+        var balance = (await UserTokenStub.GetBalance.CallAsync(new GetBalanceInput
+        {
+            Owner = SampleAccount.Accounts.Skip(1).First().Address,
+            Symbol = "ELF"
+        })).Balance;
+        balance.ShouldBe(100_00000000);
+    }
+
+    [Fact]
+    public async Task Take_Over_Amount_Test()
+    {
+        await AdminStub.Initialize.SendAsync(new InitializeInput());
+
+        await AdminTokenStub.Transfer.SendAsync(new TransferInput
+        {
+            Amount = 300_00000000,
+            Memo = "hi",
+            Symbol = "ELF",
+            To = Address.FromBase58("2LUmicHyH4RXrMjG4beDwuDsiWJESyLkgkwPdGTR8kahRzq5XS")
+        });
+
+        var executionResult = await UserStub.Take.SendWithExceptionAsync(new TakeInput
+        {
+            Symbol = "ELF",
+            Amount = 200_00000000
+        });
+
+        executionResult.TransactionResult.Error.ShouldContain("Cannot take");
+    }
+
+    [Fact]
+    public async Task Take_Twice_Test()
+    {
+        await AdminStub.Initialize.SendAsync(new InitializeInput());
+
+        await AdminTokenStub.Transfer.SendAsync(new TransferInput
+        {
+            Amount = 300_00000000,
+            Memo = "hi",
+            Symbol = "ELF",
+            To = Address.FromBase58("2LUmicHyH4RXrMjG4beDwuDsiWJESyLkgkwPdGTR8kahRzq5XS")
+        });
+
+        await UserStub.Take.SendAsync(new TakeInput
+        {
+            Symbol = "ELF",
+            Amount = 100_00000000
+        });
+
+        var executionResult = await UserStub.Take.SendWithExceptionAsync(new TakeInput
+        {
+            Symbol = "ELF",
+            Amount = 100_00000000
+        });
+
+        executionResult.TransactionResult.Error.ShouldContain("Can take");
+    }
+
+    [Fact]
+    public async Task Take_Other_Symbol_Test()
+    {
+        await AdminStub.Initialize.SendAsync(new InitializeInput());
+
+        var executionResult = await UserStub.Take.SendWithExceptionAsync(new TakeInput
+        {
+            Symbol = "ETH",
+            Amount = 100_00000000
+        });
+        executionResult.TransactionResult.Error.ShouldContain("Invalid symbol.");
     }
 }
